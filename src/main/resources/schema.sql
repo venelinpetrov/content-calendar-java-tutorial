d27cb1184df5dbf147c79c968ecd7f1c54cbb481
venelinpetrov <venelin.petrov@cubbystorage.com>
2023-09-28 18:49:16 +0300

    Connect to H2 database. Initialize with schema.sql

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS Content (
+    id INTEGER AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    desc text,
+    status VARCHAR(20) NOT NULL,
+    content_type VARCHAR(50) NOT NULL,
+    date_created TIMESTAMP NOT NULL,
+    date_updated TIMESTAMP,
+    url VARCHAR(255),
+    primary key (id)
+);
+
+
+INSERT INTO Content(title, desc, status, content_type, date_created)
+VALUES ('My post1', 'this is description', 'IDEA', 'ARTICLE', CURRENT_TIMESTAMP)
