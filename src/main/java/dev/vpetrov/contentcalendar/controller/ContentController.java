a0ac456d9c3b6c119a5cc64fffcd4f420df3d164
venelinpetrov <venelin.petrov@cubbystorage.com>
2023-09-28 11:10:29 +0300

    Add findById endpoint

--- src/main/java/dev/vpetrov/contentcalendar/controller/ContentController.java
+++ src/main/java/dev/vpetrov/contentcalendar/controller/ContentController.java
@@ -2,9 +2,12 @@ package dev.vpetrov.contentcalendar.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import dev.vpetrov.contentcalendar.model.Content;
 import dev.vpetrov.contentcalendar.repository.ContentCollectionRepository;
@@ -22,6 +25,15 @@ public class ContentController {
     @GetMapping("")
     public List<Content> findAll() {
         return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Content findById(@PathVariable(name = "id") Integer id) {
+        return repository
+            .findById(id)
+            .orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Not found")
+            );
     }
 
 }
--- src/main/java/dev/vpetrov/contentcalendar/repository/ContentCollectionRepository.java
+++ src/main/java/dev/vpetrov/contentcalendar/repository/ContentCollectionRepository.java
@@ -1,5 +1,6 @@
 package dev.vpetrov.contentcalendar.repository;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -7,6 +8,9 @@ import java.util.Optional;
 import org.springframework.stereotype.Repository;
 
 import dev.vpetrov.contentcalendar.model.Content;
+import dev.vpetrov.contentcalendar.model.Status;
+import dev.vpetrov.contentcalendar.model.Type;
+import jakarta.annotation.PostConstruct;
 
 @Repository
 public class ContentCollectionRepository {
@@ -23,5 +27,18 @@ public class ContentCollectionRepository {
             .stream()
             .filter(c -> c.id().equals(id))
             .findFirst();
+    }
+
+    @PostConstruct
+    private void init() {
+        Content c = new Content(1,
+        "My first blog post",
+        "My first blog post",
+        Status.IDEA,
+        Type.ARTICLE,
+        LocalDateTime.now(),
+        null,
+        "");
+        content.add(c);
     }
 }
